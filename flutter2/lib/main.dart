8f305a580e91665ea5a53f266bc01575dfee54da
kkm0406 <sdjmc30412@naver.com>
2022-07-03 22:45:54 +0900

    main

--- flutter2/lib/main.dart
+++ flutter2/lib/main.dart
@@ -22,6 +22,11 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(child: Text('My Page!')),
+    );
   }
 }
