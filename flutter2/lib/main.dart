efcc96cef957ff29e5202e731a7f6f8758d4c13d
kkm0406 <sdjmc30412@naver.com>
2022-07-14 18:42:56 +0900

    TextField AutoFocus

--- flutter2/lib/main.dart
+++ flutter2/lib/main.dart
@@ -12,44 +12,91 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: appTitle,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: const MyPage(),
+        body: const MyCustomForm(),
       ),
     );
   }
 }
 
-class MyPage extends StatelessWidget {
-  const MyPage({Key? key}) : super(key: key);
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  late FocusNode myFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    myFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: '이름',
-              icon: Icon(Icons.pedal_bike),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Text Field Focus'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            const TextField(
+              autofocus: true,
             ),
-          ),
+            TextField(
+              focusNode: myFocusNode,
+            ),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            decoration: InputDecoration(labelText: '나이'),
-            onChanged: (text) => print('First text field: $text'),
-          ),
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-              labelText: 'Enter your name',
-              labelStyle: TextStyle(color: Colors.purple)),
-        )
-      ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => myFocusNode.requestFocus(),
+        tooltip: 'Focus Second Text Field',
+        child: const Icon(Icons.edit),
+      ),
     );
   }
 }
+
+// class MyPage extends StatelessWidget {
+//   const MyPage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: TextField(
+//             decoration: InputDecoration(
+//               hintText: '이름',
+//               icon: Icon(Icons.pedal_bike),
+//             ),
+//           ),
+//         ),
+//         Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: TextField(
+//             decoration: InputDecoration(labelText: '나이'),
+//             onChanged: (text) => print('First text field: $text'),
+//           ),
+//         ),
+//         TextFormField(
+//           decoration: InputDecoration(
+//               labelText: 'Enter your name',
+//               labelStyle: TextStyle(color: Colors.purple)),
+//         )
+//       ],
+//     );
+//   }
+// }
